13f150a0961ddfebbf2124c47e4c84af00342d58
Jean-Christophe Sicotte-Brisson <jeanchristophesicottebrisson@gmail.com>
2022-07-24 12:37:49 -0400

    Initial commit: PCB, Gerbers and FW template

---
+++ PowerLamp_Firmware/src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include "tact.h"
+
+#define TACT_GPIO (2)
+
+tact tactSwitch = tact(2);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
